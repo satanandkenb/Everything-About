3fcd8f8f8d7e8ed73c7729c9b3a9dc1623d21a3c
Satanand <119117238+satanandkenb@users.noreply.github.com>
2025-10-25 20:28:48 +0530

    Add string manipulation examples in 05_strings.js

---
+++ javascript/01_basic/05_strings.js
@@ -0,0 +1,15 @@
+const name = "Satanad Ken"
+const repoCount = 40
+
+console.log(`Name : ${name},Repository count :${repoCount}`);
+
+const gameChange = new String("Satanand Ken")
+
+// console.log(gameChange.length);
+console.log(gameChange.charAt(5)); 
+console.log(gameChange.indexOf("K"));
+
+// Purpose                           |  Method         |  Example                 |  Description                               
+// ----------------------------------+-----------------+--------------------------+--------------------------------------------
+// Get characterat a specific index  |  charAt(index)  |  "hello".charAt(1)→'e'   |  Returns the character at position1        
+// Get theindex of a character       |  indexOf(char)  |  "hello".indexOf('e')→1  |  Returns the first index of'e'in the string 
